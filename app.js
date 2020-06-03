6ba442a8fa9836a2ce61643e1efaf40f32cfb35f
Joe Dodgson <Joe.Dodgson@arup.com>
2020-06-03 14:40:39 +0100

    Added logic to add any number of engineers and interns

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ const OUTPUT_DIR = path.resolve(__dirname, "output")
 // Move this declaration to within the user prompt, after the user has specified a file name
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const team = {};
+const team = {
+    manager : {},
+    engineers : [],
+    interns : []
+};
 
 // Define an async await function which creates a team profile HTML file based on user input
 async function createTeamTemplate() {
@@ -44,7 +48,7 @@ async function createTeamTemplate() {
             // Tell user this will overwrite existing template file. Ask if they want to continue
             const { stillContinueYN } = await inquirer.prompt({
                 type : "list",
-                message : `A file named ${fileName} already exists in the output folder.\nThis will be overwritten when you run this script.\nDo you want to continue? `, 
+                message : `A file named ${fileName}.html already exists in the output folder.\nThis will be overwritten when you run this script.\nDo you want to continue? `, 
                 name : "stillContinueYN",
                 choices : ["Yes", "No"]                
             });
@@ -79,10 +83,102 @@ async function createTeamTemplate() {
             message : "Enter the manager's office phone number: ",
             name : "officeNumber"
         }]);
-
+        
+        // Use the data entered by the user to create a new manager object using the Manager class
         const manager = new Manager(managerData.name, managerData.id, managerData.email, managerData.officeNumber);
-
+        
+        // Set the 'manager' property of the 'team' object to the newly created manager object
         team.manager = manager;
+        
+        // Ask the user if they would like to add another team member or generate the HTML page
+        let { addOrGenerate } = await inquirer.prompt({
+            type : "list",
+            message : "What would you like to do next?", 
+            name : "addOrGenerate",
+            choices : ["Add another team member","Generate team profile HTML page"]                
+        });
+        
+        while (addOrGenerate === "Add another team member") {
+            // Ask the user whether they would like to add an Engineer or Intern to the team
+            let { newMemberType } = await inquirer.prompt({
+                type : "list",
+                message : "Select a type of team member: ", 
+                name : "newMemberType",
+                choices : ["Engineer","Intern"]
+            });
+
+            if (newMemberType === "Engineer") {
+                // Prompt the user to enter information about the engineer
+                let engineerData = await inquirer.prompt([{
+                    message : "Enter the name of the engineer: ",
+                    name : "name"
+                },
+                {
+                    message : "Enter the engineer's ID number: ",
+                    name : "id"
+                },
+                {
+                    message : "Enter the engineer's email address: ",
+                    name : "email"
+                },
+                {
+                    message : "Enter the engineer's Github username: ",
+                    name : "github"
+                }]);
+                    
+                // Use the data entered by the user to create a new engineer object using the engineer class
+                let engineer = new Engineer(engineerData.name, engineerData.id, engineerData.email, engineerData.github);
+                
+                // Set the 'engineer' property of the 'team' object to the newly created engineer object
+                team.engineers.push(engineer);
+            }
+
+            else if (newMemberType === "Intern") {
+                // Prompt the user to enter information about the engineer
+                let internData = await inquirer.prompt([{
+                    message : "Enter the name of the intern: ",
+                    name : "name"
+                },
+                {
+                    message : "Enter the intern's ID number: ",
+                    name : "id"
+                },
+                {
+                    message : "Enter the intern's email address: ",
+                    name : "email"
+                },
+                {
+                    message : "Enter the intern's school: ",
+                    name : "school"
+                }]);
+                    
+                // Use the data entered by the user to create a new intern object using the intern class
+                let intern = new Intern(internData.name, internData.id, internData.email, internData.school);
+                
+                // Set the 'intern' property of the 'team' object to the newly created intern object
+                team.interns.push(intern);
+            }
+            
+            addOrGenerate = { addOrGenerate } = await inquirer.prompt({
+                type : "list",
+                message : "What would you like to do next?", 
+                name : "addOrGenerate",
+                choices : ["Add another team member","Generate team profile HTML page"]                
+            });
+        }
+        
+        // After the user has input all employees desired, call the `render` function (required
+        // above) and pass in an array containing all employee objects; the `render` function will
+        // generate and return a block of HTML including templated divs for each employee!
+        // ----------------​
+        // After you have your html, you're now ready to create an HTML file using the HTML
+        // returned from the `render` function. Now write it to a file named `team.html` in the
+        // `output` folder. You can use the variable `outputPath` above target this location.
+        // Hint: you may need to check if the `output` folder exists and create it if it
+        // does not.
+        
+        // // Give a message to tell the user the file has been created.
+        // console.log("Your team profile HTML file was created successfully!");
 
     } catch (error) {
         console.log(error);
@@ -92,29 +188,9 @@ async function createTeamTemplate() {
 // Call the createTeamTemplate function
 createTeamTemplate();
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-// ----------------​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
 
 // // Create a team profile HTML file and write the content based on user input
 // const htmlContent = 
 // `# ${answers.title}\n\n## Table of contents:\n1. Description\n2. Installation\n3. Usage\n4. Author\n5. Contributing\n6. License\n7. Tests\n8. Contact\n\n## 1. Description:\n${answers.description}\n\n## 2. Installation:\n${answers.installation}\n\n## 3. Usage:\n${answers.usage}\n\n## 4. Author:\n${answers.name}\nGithub username: ${answers.username}\n<img src="${data.avatar_url}">\n\n## 5. Contributing:\n${answers.contributing}\n\n## 6. License:\n\n${licenseShieldMD}\n\n## 7. Tests:\nThe project passed the following tests:\n${answers.tests}\n\n## 8. Contact:\nFor any questions about this project, please contact ${answers.name} at the following email address:\n${email.address}`;
 
 // const file = await writeFileAsync(filePath, htmlContent);
-
-// // Give a message to tell the user the file has been created.
-// console.log("Your team profile HTML file was created successfully!");
-
-
-// -----------------​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-// -----------------​
